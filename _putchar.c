b6b1c201705538c3a59593b66d9ef207e7649dee
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-22 17:08:54 +0300

    minor change on putchar

--- _putchar.c
+++ _putchar.c
@@ -1,15 +1,14 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - write a character to stdout
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * @c: the character to print
- *
- * Return: 1 on sucess
- *         -1 on error
-*/
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+	
