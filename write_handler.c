57235af1fb9fd91273dfe2ccc37dad7101bb95c4
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 18:00:16 +0300

    updated

--- write_handler.c
+++ write_handler.c
@@ -23,5 +23,20 @@ int flags, int width, int precision, int size)
 
 	buffer[i++] = c;
 	buffer[i] = '\0';
+	
+	if (width > 1)
+	{
+		buffer[BUFF_SIZE - 1] = '\0';
+		for (i = 0; i < width - 1; i++)
+			buffer[BUFF_SIZE - i - 2] = padd;
 
+		if (flags & F_MINUS)
+			return (write(1, &buffer[0], 1) +
+					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
+		else
+			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
+					write(1, &buffer[0], 1));
+	}
+
+	return (write(1, &buffer[0], 1));
 }
