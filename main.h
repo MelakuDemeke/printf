b5cefeefe29b53d47a0039f077aa2c86d439e68c
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:11:47 +0300

    printing hexa in capitals

--- main.h
+++ main.h
@@ -69,4 +69,6 @@ int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 #endif /* MAIN_H */
