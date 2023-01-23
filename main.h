c4f7c88cc0f1fc0f22abede9df3be06839648f17
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:42:05 +0300

    convert size

--- main.h
+++ main.h
@@ -93,5 +93,6 @@ int print_rot13string(va_list types, char buffer[],
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
+long int convert_size_number(long int num, int size);
 
 #endif /* MAIN_H */
