f79722ef8cf2994cd02bafd29ba296004a315368
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 14:22:23 +0300

    calculate size

---
+++ size_cast_arg.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_size - to calculate the size to cast the argument
+ * @format: a formatted string
+ * @n: list of arguments to be printed
+ * Return: value of precision
+ */
+int size_cast_arg(const char *format, int *n)
+{
+
+}
