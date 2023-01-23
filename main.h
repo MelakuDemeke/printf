442b84c153a693b9441cd21a9adad23a8687898b
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:15:35 +0300

    printing non-printable characters

--- main.h
+++ main.h
@@ -74,4 +74,9 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
+/* Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+
 #endif /* MAIN_H */
