b5c7c04b891c52651336349e4af8f6a7a8a3a66a
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:24:32 +0300

    printable checker is added

---
+++ utility.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated. 
+ * Return: 1 if c is printable else 0.
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
