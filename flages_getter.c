79b6bf4d100697f1a22bad6efdf17f41142aee95
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-23 14:54:41 +0300

    flags getter completed

--- flages_getter.c
+++ flages_getter.c
@@ -10,6 +10,25 @@
  */
 int get_flags(const char *format, int *i)
 {
-    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+int j, k;
+	int flags = 0;
+	const char FLAGS[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_NAME[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (k = *i + 1; format[k] != '\0'; k++)
+	{
+		for (j = 0; FLAGS[j] != '\0'; j++)
+			if (format[k] == FLAGS[j])
+			{
+				flags |= FLAGS_NAME[j];
+				break;
+			}
+
+		if (FLAGS[j] == 0)
+			break;
+	}
+
+	*i = k - 1;
+
+	return (flags);
 }
