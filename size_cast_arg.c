394df5d89bbff087ea36e152ec56a6a8e44c7fbb
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 14:30:47 +0300

    updated

--- size_cast_arg.c
+++ size_cast_arg.c
@@ -7,5 +7,18 @@
  */
 int size_cast_arg(const char *format, int *n)
 {
+   int curr_i = *n + 1;
+	int size = 0;
 
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*n = curr_i - 1;
+	else
+		*n = curr_i;
+
+	return (size);
 }
