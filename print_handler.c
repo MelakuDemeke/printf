54b0dce61c4ca8eb295374f9c013f8ec8356f880
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 16:17:30 +0300

    c s and %

--- print_handler.c
+++ print_handler.c
@@ -15,5 +15,32 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
     {
-    
-    }
+		int i, unknow_len = 0, printed_chars = -1;
+		fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
