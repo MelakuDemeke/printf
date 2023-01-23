f96fa8572c015620d87370fa11b6431b3b1ed9ef
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-23 13:14:52 +0300

    variadic start and end

--- _printf.c
+++ _printf.c
@@ -10,6 +10,17 @@
 
 int _printf(const char *format, ...)
 {
+	int i;
+
 	if (format == NULL)
 		return (-1);
+	
+	va_start(list, format);
+
+	for (i = 0; i < format; i++)
+	{
+
+	}
+
+	va_end(list);
 }
