85fabff83b2ca8a61ded50ba605992f81226d1bc
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 14:47:35 +0300

    flags are added

--- main.h
+++ main.h
@@ -12,4 +12,11 @@ int _printf(const char *format, ...);
 #define S_LONG 2
 #define S_SHORT 1
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 #endif /* MAIN_H */
