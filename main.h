5b85d58c67675ce4b31a315058b30bbacea2d97b
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:14:18 +0300

    printing hexa

--- main.h
+++ main.h
@@ -71,4 +71,7 @@ int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[],
+char buffer[], int flags, char flag_ch, int width, int precision, int size);
+
 #endif /* MAIN_H */
