a0a28d936af0338605e5eff33f813b0a2bb71e25
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:27:51 +0300

    print string in rot13

--- main.h
+++ main.h
@@ -85,6 +85,8 @@ int print_pointer(va_list types, char buffer[],
 /*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
