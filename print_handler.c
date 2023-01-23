bb7e9e6cd01b1707d72a14bda6a0304ef47ee724
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 18:13:57 +0300

    i d and b

--- print_handler.c
+++ print_handler.c
@@ -17,7 +17,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
     {
 		int i, unknow_len = 0, printed_chars = -1;
 		fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent}, {'\0', NULL}
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
