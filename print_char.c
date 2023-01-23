6b1f83622e57b160122a17a48b9e4d1bdc9ad205
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-23 16:10:42 +0300

    print char added

---
+++ print_char.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_char - Prints a char
+ *
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ *
+ * Return: Number of chars printed
+ */
+
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	/*handle write char function will be handle by teammate*/
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
