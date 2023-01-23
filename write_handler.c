da47b906c203dabf9008de770b65793084c232f5
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 18:04:40 +0300

    write number is updated

--- write_handler.c
+++ write_handler.c
@@ -56,4 +56,20 @@ int flags, int width, int precision, int size)
 int write_number(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size)
 {
+	int length = BUFF_SIZE - ind - 1;
+	char padd = ' ', extra_ch = 0;
+
+	UNUSED(size);
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+	if (is_negative)
+		extra_ch = '-';
+	else if (flags & F_PLUS)
+		extra_ch = '+';
+	else if (flags & F_SPACE)
+		extra_ch = ' ';
+
+	return (write_num(ind, buffer, flags, width, precision,
+		length, padd, extra_ch));
 }
