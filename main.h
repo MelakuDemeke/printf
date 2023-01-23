7c76bd25b43ffd808895063112747527129b773d
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:07:55 +0300

    prining unsigned number

--- main.h
+++ main.h
@@ -60,8 +60,10 @@ int print_percent(va_list types, char buffer[],
 
     /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
-int flags, int width, int precision, int size);
+    int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
-int flags, int width, int precision, int size);
+    int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
