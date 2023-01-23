507a32a04db046e932624491d2a41c5a93f150fb
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:31:56 +0300

    append the hexa code

--- utility.c
+++ utility.c
@@ -11,3 +11,26 @@ int is_printable(char c)
 
 	return (0);
 }
+
+/**
+ * append_hexa_code -  Append ascci in hexadecimal code to buffer
+ * @ascii_code: ASSCI CODE
+ * @buffer: Array of chars.
+ * @i: Index at which to start appending.
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
