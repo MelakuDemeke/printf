5822e49da1879acc2013a266cbaee95a3c9588a1
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 17:05:49 +0300

    printing binnary

--- main.h
+++ main.h
@@ -60,6 +60,8 @@ int print_percent(va_list types, char buffer[],
 
     /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
