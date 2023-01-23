887e1615ecc727cb24a3834e650d31fec3e1c6a9
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-23 15:20:46 +0300

    get width added

--- widht_getter.c
+++ widht_getter.c
@@ -12,5 +12,27 @@
 
 int get_width(const char *format, int *i, va_list list)
 {
+    	int k;
+	int width = 0;
 
+	for (k = *i + 1; format[k] != '\0'; k++)
+	{
+		if (is_digit(format[k]))
+		{
+			width *= 10;
+			width += format[k] - '0';
+		}
+		else if (format[k] == '*')
+		{
+			k++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = k - 1;
+
+	return (width);
 }
