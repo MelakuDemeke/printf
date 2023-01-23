bd93c8a24379bc58e32a568e2448c369e54e1cd4
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-23 14:49:17 +0300

    define flags ch and flags array

--- flages_getter.c
+++ flages_getter.c
@@ -10,5 +10,6 @@
  */
 int get_flags(const char *format, int *i)
 {
-
+    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 }
