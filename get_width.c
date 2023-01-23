daeb4503d9428426608a2d0955da91fdef25e2a6
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-01-23 14:42:02 +0300

    prototype for calculating width for printing is defined

---
+++ get_width.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_width: calculate width for printing
+ * @format: formatted string
+ * @n: list of arguments
+ * @lsit: list of arguments
+ * Return: width value
+ */
+int get_width(const char *format, int *i, va_list list)
+{
+}
