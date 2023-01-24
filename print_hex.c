09994ad578b18d1826d3185d1f0b62a71d3b8210
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-24 10:33:45 +0300

    betty fix

--- print_hex.c
+++ print_hex.c
@@ -56,7 +56,7 @@ int print_hexa_upper(va_list types, char buffer[],
  *
  * Return: Number of chars printed
  */
- 
+
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
