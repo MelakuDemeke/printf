ba41c68d7525c5bd00c794aa756a1b9876b70aeb
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-24 10:44:07 +0300

    betty fix

--- utility.c
+++ utility.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated. 
+ * @c: Char to be evaluated.
  * Return: 1 if c is printable else 0.
  */
 int is_printable(char c)
