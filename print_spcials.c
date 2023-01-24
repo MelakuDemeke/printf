ac6f0862bdba639d4e647111733a43efb07167f8
Melaku Demeke <melaku.demeke789@gmail.com>
2023-01-24 10:38:42 +0300

    bettyfix

--- print_spcials.c
+++ print_spcials.c
@@ -16,7 +16,7 @@ int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	char extra_c = 0, padd = ' ';
-	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; 
+	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1;
 	unsigned long num_addrs;
 	char map_to[] = "0123456789abcdef";
 	void *addrs = va_arg(types, void *);
